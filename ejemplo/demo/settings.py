902a5ac24587acc54ff92ef76abdd68da266d378
mario alberto <graff_rasmus@hotmail.com>
2014-12-17 23:07:51 -0600

    mas comentarios

---
+++ ejemplo/demo/settings.py
@@ -0,0 +1,17 @@
+<open file '/home/flor/proyectos/django_plus/demo/demo/settings.py', mode 'r' at 0x330a030>
+STATICFILES_DIR = 
+(
+	'static/',
+)
+
+
+TEMPLATE_DIRS =
+(
+	'plantillas/',
+)
+
+MEDIA_URL = '/media/'
+MEDIR_ROOT = 
+(
+	'media/',
+)
